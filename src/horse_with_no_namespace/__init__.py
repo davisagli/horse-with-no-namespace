aec69f89fd20f267a71b0d6219b5384f71704d20
David Glick <david@glicksoftware.com>
2025-04-08 13:01:41 -0700

    Log to stderr rather than stdout

--- src/horse_with_no_namespace/__init__.py
+++ src/horse_with_no_namespace/__init__.py
@@ -17,15 +17,19 @@ import sys
 
 logged = False
 
+
 def apply():
     # The Python site module can call us more than once.
     # We need to actually do this the last time,
     # But we only want to show the notice once.
     global logged
     if not logged:
-        print("🐎 This Python uses horse-with-no-namespace "
-                "to make pkg_resources namespace packages compatible "
-                "with PEP 420 namespace packages.")
+        print(
+            "🐎 This Python uses horse-with-no-namespace "
+            "to make pkg_resources namespace packages compatible "
+            "with PEP 420 namespace packages.",
+            file=sys.stderr,
+        )
         logged = True
 
     # Only patch pkg_resources if it is installed...
@@ -38,12 +42,15 @@ def apply():
         # to update __path__ using pkgutil.extend_path instead
         def declare_namespace(packageName):
             parent_locals = sys._getframe(1).f_locals
-            parent_locals["__path__"] = pkgutil.extend_path(parent_locals["__path__"], packageName)
+            parent_locals["__path__"] = pkgutil.extend_path(
+                parent_locals["__path__"], packageName
+            )
+
         pkg_resources.declare_namespace = declare_namespace
 
     # Remove existing namespace package modules that were already created
     # by other .pth files, possibly with an incomplete __path__
     for name, module in list(sys.modules.items()):
         loader = getattr(module, "__loader__", None)
-        if loader and loader.__class__.__name__ == 'NamespaceLoader':
+        if loader and loader.__class__.__name__ == "NamespaceLoader":
             del sys.modules[name]
