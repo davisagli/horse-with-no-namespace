f5765904f5c353d9241aa5b2d3b77db747fba8c9
ale-rt <alessandro.pisa@gmail.com>
2025-06-26 17:46:46 +0200

    Minimal test buildout

---
+++ demo_buildout/test.py
@@ -0,0 +1,9 @@
+import five.globalrequest  # noqa: F401
+import five.localsitemanager  # noqa: F401
+
+print("")
+print("Testing five.globalrequest and five.localsitemanager imports...")
+print("")
+
+print(five.globalrequest)
+print(five.localsitemanager)
