08712c96f0719aef3ddad56698c5ffc721f48038
David Glick <david@glicksoftware.com>
2025-07-04 14:55:27 -0700

    Fix problems when using horse-with-no-namespace with zc.buildout.
    1. Buildout recipes are loaded via a pkg_resources API that updates sys.path. We have to make sure that the __path__ for a namespace module gets updated when sys.path changes.
    2. Scripts installed by buildout do their own mangling of sys.path. We have to make sure that the pkg_resources working set doesn't get initialized until after that.

--- demo_buildout/test.py
+++ demo_buildout/test.py
@@ -1,9 +1,9 @@
-import five.globalrequest  # noqa: F401
-import five.localsitemanager  # noqa: F401
+import zc.buildout  # noqa: F401
+import zc.lockfile  # noqa: F401
 
 print("")
-print("Testing five.globalrequest and five.localsitemanager imports...")
+print("Testing imports...")
 print("")
 
-print(five.globalrequest)
-print(five.localsitemanager)
+print(zc.buildout)
+print(zc.lockfile)
